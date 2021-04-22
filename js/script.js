561b1494b1483b9c9cd18bc00c506b95d0e2e56c
Yakub <yakub.egamnazarov@mail.ru>
2021-04-22 17:28:24 +0800

    Animation + Responsive Design
    Animation CCS added to Hero Section
    Responsive Design Complete for 4 breakpoints, 1200px, 900px, 700px, 400px

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// const toggleBtn = document.querySelector('.toggle-btn');
+// const navbar = document.querySelector('.navbar');
+
+// toggleBtn.addEventListener('click', () => {
+//     navbar.classList.toggle('active');
+//     console.log('active class added');
+// })
